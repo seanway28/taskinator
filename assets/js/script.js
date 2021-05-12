61b0cd51eadf6622f4e563e4473021b17061df46
seanway28 <seanway28@gmail.com>
2021-05-11 22:05:23 -0400

    basic add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
-window.document.querySelector("button");
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+const createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
